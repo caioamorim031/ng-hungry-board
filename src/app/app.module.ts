88b8d4b8faa620b96ba3a4d461c60e0bc0fab84a
CAIO CESAR CARVALHO AMORIM <caio.amorim@sefaz.pe.gov.br>
2020-01-24 16:48:35 -0300

    Primeiro Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AppHeaderComponent } from './app-header/app-header.component';
+import { AppFooterComponent } from './app-footer/app-footer.component';
+import { AppHomeComponent } from './app-home/app-home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppHeaderComponent,
+    AppFooterComponent,
+    AppHomeComponent
   ],
   imports: [
     BrowserModule
